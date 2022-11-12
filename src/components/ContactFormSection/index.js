bc29ef20ba4907617683fbb86e93c6def277dbd9
franpautt27 <franpautt@gmail.com>
2022-11-12 12:46:29 -0500

    Add Email Web service

--- src/components/ContactFormSection/index.js
+++ src/components/ContactFormSection/index.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useRef} from "react";
+import emailjs from '@emailjs/browser';
+import toast from 'react-simple-toasts';
 import {
   ContactContainer,
   ContactWrapper,
@@ -20,6 +22,23 @@ import {
 } from "./ContactFormElements";
 
 const ContactForm = () => {
+  const formRef = useRef()
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_x7v7g4a', 'template_4pasxzr', formRef.current,"cnx4LQIhVIT3iWYKk")
+      .then((result) => {
+          console.log(result.text);
+          e.target.reset()
+          toast("Formulario enviado exitosamente!")
+      }, (error) => {
+          console.log(error.text);
+          toast("Error enviando el formulario")
+      });
+
+  }
+
   return (
     <>
       <ContactContainer id="contactenos">
@@ -34,13 +53,13 @@ const ContactForm = () => {
             </ContactColumn1>
             <ContactColumn2>
               <FormWrap>
-                <Form>
-                  <NameInput placeholder="Nombre Completo"></NameInput>
-                  <EmailInput placeholder="Email"></EmailInput>
-                  <ServiceSelect placeholder="Servicio requerido"></ServiceSelect>
-                  <SubjectInput placeholder="Asunto"></SubjectInput>
-                  <MessageInput rows="5" placeholder="Mensaje"></MessageInput>
-                  <ContactButton>Enviar</ContactButton>
+                <Form ref={formRef} onSubmit={handleSubmit} >
+                  <NameInput required={true} type="text" name="nombre" placeholder="Nombre Completo"></NameInput>
+                  <EmailInput required={true} type="email" name="email" placeholder="Email"></EmailInput>
+                  <ServiceSelect required={true} type="text" name="servicio"  placeholder="Servicio requerido"></ServiceSelect>
+                  <SubjectInput required={true} type="text" name="asunto" placeholder="Asunto"></SubjectInput>
+                  <MessageInput required={true} type="text" name="mensaje" rows="5" placeholder="Mensaje"></MessageInput>
+                  <ContactButton type="submit">Enviar</ContactButton>
                 </Form>
               </FormWrap>
             </ContactColumn2>
